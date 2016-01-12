bf8ef4c883994376a718994c7ad923c373b57d1a
Tobias <tobias.f.wolf@me.com>
2016-01-12 14:14:06 +0100

    better search and pagination

--- site/templates/film-archive.php
+++ site/templates/film-archive.php
@@ -3,13 +3,48 @@
 $start = time();
 
 $db = new Db();
-// $db->insert($page);
-$results = $db->films->order('country DESC')
-                     ->all();
-print_r($results->toJson());
-
 // $db->insertExisiting();
 
+// pagination
+$page = 1;
+if (array_key_exists('page',url::params())) {
+  $page = url::params()['page'];
+}
 
-$end = time();
-print_r($end - $start);
+$films = $db->films;
+
+if ($search = get('search')) {
+  $films = $films->where(  'uri',      'LIKE', '%'.$search.'%')
+                 ->orWhere('title',    'LIKE', '%'.$search.'%')
+                 ->orWhere('still',    'LIKE', '%'.$search.'%')
+                 ->orWhere('type',     'LIKE', '%'.$search.'%')
+                 ->orWhere('country',  'LIKE', '%'.$search.'%')
+                 ->orWhere('year',     'LIKE', '%'.$search.'%')
+                 ->orWhere('duration', 'LIKE', '%'.$search.'%')
+                 ->orWhere('director', 'LIKE', '%'.$search.'%')
+                 ->orWhere('text',     'LIKE', '%'.$search.'%');
+}
+
+$films = $films->order('uri ASC')->page($page, 100);
+$pagination = $films->pagination();
+$pagination->nextPageURL();
+
+$duration = time()-$start;
+
+$output = array(
+            'count' => $films->count(),
+            'films' => $films->toArray(),
+          );
+
+if ($pagination) {
+  $paginationArray = $pagination->toArray();
+  if ($pagination->nextPageUrl() && $pagination->nextPageUrl() != url::current()) {
+    $paginationArray['nextPageUrl'] = $pagination->nextPageUrl();
+  }
+  if ($pagination->prevPageURL() && $pagination->prevPageURL() != url::current()) {
+    $paginationArray['prevPageURL'] = $pagination->prevPageURL();
+  }
+  $output['pagination'] = $paginationArray;
+}
+
+echo a::json($output);
