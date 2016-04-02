023711eab41367b8168a8854c43361a1fd176f57
Tobias <tobias.f.wolf@me.com>
2016-04-02 17:48:35 +0200

    Timetable

--- assets/js/script.js
+++ assets/js/script.js
@@ -398,15 +398,26 @@ if (document.body.classList.contains('filmblock')) {
 function timetable() {
 
   var eventElements = document.querySelectorAll('.timetable .day .event');
+  var hoursSpanElements = document.querySelectorAll('.timetable .hours span');
 
   _.each(eventElements, function(element){
     var hours = element.dataset.time.substr(0, 2);
     var minutes = element.dataset.time.substr(3);
-    var top = ((hours - 17) + (minutes / 60)) * 4;
+    var top = ((hours - 15.5) + (minutes / 60)) * 4;
     var duration = element.dataset.duration;
     var height = duration * 4;
-    element.style.top = top + 'em';
-    element.style.height = height + 'em';
+    element.style.top = 'calc(' + top + 'rem + 2px)';
+    element.style.height = 'calc(' + height + 'rem - 2px)';
+  });
+
+  _.each(hoursSpanElements, function(element){
+    var hours = element.innerHTML.substr(0, 2);
+    if (hours === '00') {
+      hours = 24;
+    }
+    var minutes = element.innerHTML.substr(3);
+    var top = ((hours - 15.5) + (minutes / 60)) * 4;
+    element.style.top = top + 'rem';
   });
 
 }
--- site/templates/timetable.php
+++ site/templates/timetable.php
@@ -5,13 +5,18 @@
 <?php snippet('mainnav') ?>
 
 <?php
-global $page;
-function returnDay($day) {
+function returnDay($day, $faulloch = false) {
   $day = strtotime($day);
   $html = '<div class="day">
     <header>
-      <h1>Di <span class="date">' . date('d', $day) . '<br>' . date('m', $day) .'</span></h1>
-    </header>';
+      <h1><span>' . strftime('%a', $day) . '</span><time datetime="' . date('c', $day) . '">' . date('d', $day) . '<br>' . date('m', $day) .'</time></h1>';
+  if ($faulloch) {
+    $html .= '<h2 class="black span">Faulloch<br>Johannistor</h2>';
+  } else {
+    $html .= '<h2>Großer Saal</h2>
+    <h2 class="black">Kleiner Saal</h2>';
+  }
+  $html .= '</header>';
   $events = page()->events()->toStructure()->filter(function($field) use ($day) {
     return $field->date('d.m.y', 'datetime') == date('d.m.y', $day);
   });
@@ -33,6 +38,8 @@ function returnEvent($event) {
       $element->attr('title', $page->title() . ' – ' . $page->subtitle());
     }
   }
+  $element->attr('itemscope');
+  $element->attr('itemtype', 'http://schema.org/Event');
   $element->addClass('event');
   if ($event->black()->bool()) {
     $element->addClass('black');
@@ -43,6 +50,25 @@ function returnEvent($event) {
   $element->data('time', $event->date('H:i', 'datetime'));
   $element->data('duration', $event->duration());
   $element->html($event->name()->kt());
+  $element->append('<meta itemprop="organizer" content="' . site()->title() . '">');
+  if (!page()->venue()->isEmpty()) {
+    $elementLocation = new Brick('span');
+    $elementLocation->attr('itemprop', 'location');
+    $elementLocation->attr('itemscope', ' ');
+    $elementLocation->attr('itemtype', 'http://schema.org/Place');
+    $elementLocation->html('<meta itemprop="name" content="' . page()->venue() . '">');
+    if (!page()->street()->isEmpty() AND !page()->zip()->isEmpty() AND !page()->city()->isEmpty()) {
+      $elementAddress = new Brick('span');
+      $elementAddress->attr('itemprop', 'address');
+      $elementAddress->attr('itemscope', ' ');
+      $elementAddress->attr('itemtype', 'http://schema.org/PostalAddress');
+      $elementAddress->html('<meta itemprop="streetAddress" content="' . page()->street() . '">');
+      $elementAddress->append('<meta itemprop="postalCode" content="' . page()->zip() . '">');
+      $elementAddress->append('<meta itemprop="addressLocality" content="' . page()->city() . '">');
+      $elementLocation->append($elementAddress);
+    }
+    $element->append($elementLocation);
+  }
   return $element->toString();
 }
 ?>
@@ -51,14 +77,40 @@ function returnEvent($event) {
   <main>
     <div class="table">
       <div class="hours">
+        <span>17:00</span>
+        <span class="black">17:15</span>
+        <span>18:00</span>
+        <span class="black">18:15</span>
+        <span>19:00</span>
+        <span class="black">19:15</span>
+        <span>20:00</span>
+        <span class="black">20:15</span>
+        <span>21:00</span>
+        <span class="black">21:15</span>
+        <span>22:00</span>
+        <span class="black">22:15</span>
+        <span>23:00</span>
+        <span>00:00</span>
       </div>
-      <?= returnDay('2016-04-19') ?>
+      <?= returnDay('2016-04-19', $faulloch = true) ?>
       <?= returnDay('2016-04-20') ?>
       <?= returnDay('2016-04-21') ?>
       <?= returnDay('2016-04-22') ?>
       <?= returnDay('2016-04-23') ?>
       <?= returnDay('2016-04-24') ?>
     </div>
+    <footer>
+      <?php if(!$page->footer_1()->empty()): ?>
+      <div class="footer_1">
+        <?= $page->footer_1()->kt() ?>
+      </div>
+      <?php endif ?>
+      <?php if(!$page->footer_2()->empty()): ?>
+      <div class="footer_2">
+        <?= $page->footer_2()->kt() ?>
+      </div>
+      <?php endif ?>
+    </footer>
   </main>
 </div>
 
