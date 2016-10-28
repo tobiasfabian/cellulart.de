ee5f18320b38c3df2b870702dd8a0ac8da4d9e94
Tobias <tobias.f.wolf@me.com>
2016-10-28 14:34:38 +0200

    homepage without text elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -252,65 +252,13 @@ _.each(galleryElements,function(e){
 
 function homepage() {
 
-  var overlayElement = document.querySelector('.overlay');
-  var celluElement = overlayElement.querySelector('.cellu');
-  var lartElement = overlayElement.querySelector('.lart');
-  var dateElement = overlayElement.querySelector('.date');
-  var venueElement = overlayElement.querySelector('.venue');
-  var logoElement = document.querySelector('.mainfooter .logo');
   var videoElement = document.querySelector('.teaser video');
-  var imgElement = document.querySelector('.teaser img');
-
-  function showElementsWithTimeouts() {
-    videoElement.style.display = 'none';
-    imgElement.style.display = 'block';
-    setTimeout(function(){
-      celluElement.style.opacity = '1';
-      lartElement.style.opacity = '1';
-    },1*1000);
-    setTimeout(function(){
-      dateElement.style.opacity = '1';
-    },2.5*1000);
-    setTimeout(function(){
-      venueElement.style.opacity = '1';
-    },2.7*1000);
-    setTimeout(function(){
-      logoElement.style.transition = '1s';
-      logoElement.offsetWidth;
-      logoElement.style.opacity = '1';
-    },2.9*1000);
-  }
-
-  var videoTimeout = setTimeout(showElementsWithTimeouts,3*1000);
 
   if (videoElement.offsetWidth !== 0) {
     videoElement.addEventListener('canplaythrough',function(){
       this.play();
     });
-    videoElement.addEventListener('timeupdate',function(){
-      clearTimeout(videoTimeout);
-      if (this.currentTime > 1.4) {
-        dateElement.style.opacity = '1';
-      }
-      if (this.currentTime > 2) {
-        venueElement.style.opacity = '1';
-      }
-      if (this.currentTime > 4.8) {
-        celluElement.style.opacity = '1';
-        lartElement.style.opacity = '1';
-      }
-      if (this.currentTime > 6.8) {
-        logoElement.style.transition = '1s';
-        logoElement.offsetWidth;
-        logoElement.style.opacity = '1';
-      }
-    });
-  } else {
-    showElementsWithTimeouts();
   }
-
-
-
 
 }
 
